6e6a1825aaf566f5a91f312e55cc4350d6e1f9d9
Filip Strajnar <filip.strajnar@gmail.com>
2024-09-21 23:18:04 +0200

    Auto formatting.

--- src/raspi_relay_rest/__init__.py
+++ src/raspi_relay_rest/__init__.py
@@ -7,9 +7,11 @@ import gpiozero
 
 app = Flask(__name__)
 
+
 @app.route("/ping")
 def hello_world():
     return "pong"
+
 
 @app.route("/led/<int:number>/<state>")
 def led_state(number: int, state: str):
@@ -18,15 +20,16 @@ def led_state(number: int, state: str):
         led.on()
     if state == "off":
         led.off()
-    
+
     led_value = led.value
     led.close()
     return str(led_value)
+
 
 @app.route("/led-blink/<int:number>")
 def led_blink(number: int):
     led = gpiozero.LED(number, active_high=False)
     blink_duration: int = int(request.args.get("duration", 1))
-    led.blink(blink_duration, n = 1, background=False)
+    led.blink(blink_duration, n=1, background=False)
     led.close()
     return str(blink_duration)
