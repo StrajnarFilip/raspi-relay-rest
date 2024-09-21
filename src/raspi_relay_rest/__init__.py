40826b92917b5396011001b9f0251f571bc25316
Filip Strajnar <filip.strajnar@gmail.com>
2024-09-21 20:42:39 +0200

    Endpoint for quick check if REST API is active.

--- src/raspi_relay_rest/__init__.py
+++ src/raspi_relay_rest/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-FileCopyrightText: 2024-present Filip Strajnar <filip.strajnar@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/ping")
+def hello_world():
+    return "pong"
