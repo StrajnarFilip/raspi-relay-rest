7614b7517b15eeeb6025a85410fd7c8d776f332a
Filip Strajnar <filip.strajnar@gmail.com>
2024-09-21 22:10:03 +0200

    Created endpoint to change LED state.

--- src/raspi_relay_rest/__init__.py
+++ src/raspi_relay_rest/__init__.py
@@ -3,9 +3,22 @@
 # SPDX-License-Identifier: MIT
 
 from flask import Flask
+import gpiozero
 
 app = Flask(__name__)
 
 @app.route("/ping")
 def hello_world():
     return "pong"
+
+@app.route("/led/<int:number>/<state>")
+def led_state(number: int, state: str):
+    led = gpiozero.LED(number)
+    if state == "on":
+        led.on()
+        return led.value
+    if state == "off":
+        led.off()
+        return led.value
+    
+    return "State may only be 'on' or 'off'."
